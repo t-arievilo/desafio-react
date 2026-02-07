2d1c6f204cd0c910b287dd88d8bc8b33725f67c7
Thiago Oliveira <thiago.math@hotmail.com>
2026-02-07 20:29:08 -0300

    modifica nome de propriedade da Model Usuario

---
+++ backend/Migrations/20260207232647_RenameSennhaSalt.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutenticacaoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameSennhaSalt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "senhaSalt",
+                table: "Usuario",
+                newName: "SenhaSalt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SenhaSalt",
+                table: "Usuario",
+                newName: "senhaSalt");
+        }
+    }
+}
--- backend/Models/UsuarioModel.cs
+++ backend/Models/UsuarioModel.cs
@@ -11,7 +11,7 @@ namespace Models
         public string Email { get; set; }
         public string Usuario { get; set; }
         public byte[] SenhaHash { get; set; }
-        public byte[] senhaSalt { get; set; }
+        public byte[] SenhaSalt { get; set; }
         public bool Status { get; set; }
         public DateTime TokenDataCriacao { get; set; } = DateTime.Now;
     }
