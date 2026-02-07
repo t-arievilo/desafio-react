e994f45c42ff7a5ecb7f2e2bf9eb14d208a4f8f6
Thiago Oliveira <thiago.math@hotmail.com>
2026-02-07 20:06:11 -0300

    cria DTO para registro de usuario

---
+++ backend/DTOs/UsuarioCriacaoDto.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.DTOs
+{
+    public class UsuarioCriacaoDto
+    {
+        [Required(ErrorMessage = "O campo usuário é obrigatório")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "O campo usuário é obrigatório"), EmailAddress(ErrorMessage = "Formato de Email inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo senha é obrigatório")]
+        public string Senha { get; set; }
+
+        [Compare("Senha", ErrorMessage = "As senhas não são iguais")]
+        public string ConfirmaSenha { get; set; }  
+        public bool Status { get; set; } = true; 
+
+    }
+}
