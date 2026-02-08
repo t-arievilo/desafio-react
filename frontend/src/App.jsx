395a9a91e8c94c10dd210b8fa2755fda45f6720e
Thiago Oliveira <thiago.math@hotmail.com>
2026-02-08 15:07:37 -0300

    implementa rota para dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Registro from "./pages/Registro";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/registro" element={<Registro />} />
-        <Route path="login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,4 @@
+function Dashboard() {
+  return <h1>olá mundo</h1>;
+}
+export default Dashboard;
